81b20c4e2f9854d8356c8ca3e2e9806708f61a21
public-release <as-public-release@janestreet.com>
2025-10-01 21:40:11 +0100

    v0.18~preview.130.55+197

--- runtime/ppx_hardcaml_runtime.ml
+++ runtime/ppx_hardcaml_runtime.ml
@@ -1,52 +1,16 @@
 open Base
-
-module Array = struct
-  include Array
-
-  let for_ length ~f =
-    for i = 0 to length - 1 do
-      f i
-    done
-  ;;
-end
-
-module Iarray = struct
-  include Iarray
-
-  let for_ = Array.for_
-end
-
-module Int = Int
+include Ppx_hardcaml_runtime0
 module Interface = Hardcaml.Interface
-module List = List
-
-let concat = String.concat
-
-let option_map2_exn a b ~f =
-  match a, b with
-  | None, None -> None
-  | Some a, Some b -> Some (f a b)
-  | _, _ ->
-    raise_s [%message "Option.map2 expects either both to be Some, or both to be None!"]
-;;
-
-let option_iter2_exn a b ~f =
-  match a, b with
-  | None, None -> ()
-  | Some a, Some b -> f a b
-  | _, _ ->
-    raise_s [%message "Option.map2 expects either both to be Some, or both to be None!"]
-;;
 
 module Derive_interface_from_map2 (X : sig
-    type 'a t
+    type 'a t [@@deriving equal ~localize, compare ~localize]
 
     val sexp_of_t : ('a -> Sexplib0.Sexp.t) -> 'a t -> Sexplib0.Sexp.t
     val port_names_and_widths : (string * int) t
     val map2 : 'a t -> 'b t -> f:('a -> 'b -> 'c) -> 'c t
   end) =
 Interface.Make (struct
-    type 'a t = 'a X.t
+    type 'a t = 'a X.t [@@deriving equal ~localize, compare ~localize]
 
     let sexp_of_t = X.sexp_of_t
     let port_names_and_widths = X.port_names_and_widths

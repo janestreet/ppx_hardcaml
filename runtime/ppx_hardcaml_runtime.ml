c8af3c30a2b0c6571ccb4a341822691d6d209961
public-release <as-public-release@janestreet.com>
2025-11-12 20:37:29 +0000

    v0.18~preview.130.74+175

--- runtime/ppx_hardcaml_runtime.ml
+++ runtime/ppx_hardcaml_runtime.ml
@@ -1,27 +1,31 @@
 open Base
 include Ppx_hardcaml_runtime0
 module Interface = Hardcaml.Interface
+module Wave_format = Hardcaml.Wave_format
 
 module Derive_interface_from_map2 (X : sig
     type 'a t [@@deriving equal ~localize, compare ~localize]
 
     val sexp_of_t : ('a -> Sexplib0.Sexp.t) -> 'a t -> Sexplib0.Sexp.t
     val port_names_and_widths : (string * int) t
-    val map2 : 'a t -> 'b t -> f:('a -> 'b -> 'c) -> 'c t
+    val map2 : 'a t -> 'b t -> f:('a -> 'b -> 'c) @ local -> 'c t
+    val wave_formats : Wave_format.t t
   end) =
-Interface.Make (struct
+Interface.Make_with_wave_formats (struct
     type 'a t = 'a X.t [@@deriving equal ~localize, compare ~localize]
 
     let sexp_of_t = X.sexp_of_t
     let port_names_and_widths = X.port_names_and_widths
-    let map2 s t ~f = X.map2 s t ~f
-    let map t ~f = map2 t t ~f:(fun t _ -> f t)
-    let iter2 s t ~f = ignore (map2 s t ~f:(fun s t -> f s t) : unit t)
-    let iter t ~f = ignore (map2 t t ~f:(fun t _ -> f t) : unit t)
+    let map2 s t ~(f @ local) = X.map2 s t ~f [@nontail]
+    let map t ~(f @ local) = map2 t t ~f:(fun t _ -> f t) [@nontail]
+    let iter2 s t ~(f @ local) = ignore (map2 s t ~f:(fun s t -> f s t) : unit t)
+    let iter t ~(f @ local) = ignore (map2 t t ~f:(fun t _ -> f t) : unit t)
 
     let to_list t =
       let x = ref [] in
       iter t ~f:(fun t -> x := t :: !x);
       List.rev !x
     ;;
+
+    let wave_formats = X.wave_formats
   end)
